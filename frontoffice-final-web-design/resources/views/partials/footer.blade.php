504b4226b5c8d2fef24a69b3594c0c42f3073cb7
God Johary <godjohary@gmail.com>
2023-05-09 00:52:02 +0300

    Update footer.blade.php
    Give discernible text to links

--- frontoffice-final-web-design/resources/views/partials/footer.blade.php
+++ frontoffice-final-web-design/resources/views/partials/footer.blade.php
@@ -9,12 +9,12 @@
                 <div class="widget">
                     <h3>Social</h3>
                     <ul class="list-unstyled social">
-                        <li><a href="https://www.instagram.com/explore/tags/artificialintelligence/" id="instagramIcon"><span class="icon-instagram"></span></a></li>
-                        <li><a href="https://twitter.com/i/topics/898262889378619392" id="twitterIcon"><span class="icon-twitter"></span></a></li>
-                        <li><a href="https://www.facebook.com/watch/?v=431521780936760" id="facebookIcon"><span class="icon-facebook"></span></a></li>
-                        <li><a href="https://www.linkedin.com/pulse/artificial-intelligence-melkart-je?trk=organization_guest_main-feed-card_feed-article-content" id="linkedinIcon"><span class="icon-linkedin"></span></a></li>
-                        <li><a href="https://www.pinterest.com/pin/232568768242937089/" id="pinterestIcon"><span class="icon-pinterest"></span></a></li>
-                        <li><a href="https://dribbble.com/tags/ai_artificial_intelligence" id="dribbbleIcon"><span class="icon-dribbble"></span></a></li>
+                        <li><a href="https://www.instagram.com/explore/tags/artificialintelligence/" aria-label="See more about Artificial Intelligence" id="instagramIcon"><span class="icon-instagram"></span></a></li>
+                        <li><a href="https://twitter.com/i/topics/898262889378619392" aria-label="Read more about trending Artificial Intelligence tweets" id="twitterIcon"><span class="icon-twitter"></span></a></li>
+                        <li><a href="https://www.facebook.com/watch/?v=431521780936760" aria-label="Learn more about Artificial Intelligence" id="facebookIcon"><span class="icon-facebook"></span></a></li>
+                        <li><a href="https://www.linkedin.com/pulse/artificial-intelligence-melkart-je?trk=organization_guest_main-feed-card_feed-article-content" aria-label="Read more about Artificial Intelligence" id="linkedinIcon"><span class="icon-linkedin"></span></a></li>
+                        <li><a href="https://www.pinterest.com/pin/232568768242937089/" aria-label="Check more posts about Artificial Intelligence" id="pinterestIcon"><span class="icon-pinterest"></span></a></li>
+                        <li><a href="https://dribbble.com/tags/ai_artificial_intelligence" aria-label="See more design about Artificial Intelligence" id="dribbbleIcon"><span class="icon-dribbble"></span></a></li>
                     </ul>
                 </div> <!-- /.widget -->
             </div> <!-- /.col-lg-4 -->
